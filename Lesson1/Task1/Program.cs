4e449eafab8eb4f15f104ff285555339a26d34bf
PJS <b_rok9999@mail.ru>
2024-03-06 22:45:06 +0500

    Add Task1
    Add Hello, World!
    Add some variables

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// Console.WriteLine("Hello, World!");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
