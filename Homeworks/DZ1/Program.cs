3e6c833f50ddcca6eb811ffc7ad1006787822c89
PJS <b_rok9999@mail.ru>
2024-03-09 18:08:52 +0500

    Add Homeworks/DZ1

---
+++ Homeworks/DZ1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Большее из двух чисел
+// Внутри класса Answer напишите метод CompareNumbers, который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+// Пример использования:
+// На входе:
+// firstNumber: '5'
+// secondNumber: '6'
+// На выходе:
+// второе число больше
+
+// На входе:
+// firstNumber: '6'
+// secondNumber: '3'
+// На выходе:
+// первое число больше
+
+// На входе:
+// firstNumber: '3'
+// secondNumber: '3'
+// На выходе:
+// числа равны
+
+System.Console.WriteLine("Input firstNumber: ");
+System.Console.WriteLine("Input secondNumber: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber)
+{
+    System.Console.WriteLine("Числа равны");
+}
+else
+{
+    if (firstNumber > secondNumber)
+    {
+        System.Console.WriteLine("Первое число больше");
+    }
+    else
+    {
+        System.Console.WriteLine("Второе число больше");
+    }
+}
