efe32eecd6aa72e474c6a8696dd3e96c6ee67c51
PJS <b_rok9999@mail.ru>
2024-03-11 14:11:15 +0500

    Add Seminars, Task1,2

---
+++ Seminars/Sem2/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+System.Console.WriteLine("Введи трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int delete = 1;
+var str = number.ToString();
+number = int.Parse(str.Remove(delete, 1));
+int result = Convert.ToInt32(number);
+System.Console.WriteLine(result);
---
+++ Seminars/Sem2/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+System.Console.WriteLine("Введи трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int baseNumber = (number % 100) / 10;
+    int exponent = number % 10;
+    double result = Convert.ToDouble(Math.Pow(baseNumber, exponent));
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("You input is not three-digit number.");
+}
+
+
+
+
+// for (int i = 0; i < exponent; i++) {
+//     result *= baseNumber;
+// }
+
+// Console.WriteLine(result);
