2e3d1b756feb51af50e4cdfee45e2a590b268659
PJS <b_rok9999@mail.ru>
2024-03-13 14:48:16 +0500

    Add Lesson2, Task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Создать массив и заполнить его целыми числами
+// от 1 до N (N - произвольное натуральное число).
+// Вывести элементы массива на экран.
+
+// Пример N = 4
+// 1 2 3 4
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    // System.Console.Write(arr[i]);
+    // System.Console.Write(' ');
+    System.Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
