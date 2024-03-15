dc5df27f88dde56b62ca740553ff41734ded1f4d
PJS <b_rok9999@mail.ru>
2024-03-15 19:06:19 +0500

    Add Homeworks, DZ3, Task2,3

--- Homeworks/DZ3/Task1/Program.cs
+++ Homeworks/DZ3/Task1/Program.cs
@@ -11,48 +11,108 @@
 // maxRange - максимальная граница диапазона
 // public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
 
-int[] CreateArray(int n, int minRange, int maxRange)
-{
-    int[] numbers = new int[n];
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        numbers[i] = new Random().Next(minRange, maxRange + 1);
-    }
-    return numbers;
-}
+// int[] CreateArray(int n, int minRange, int maxRange)
+// {
+//     int[] numbers = new int[n];
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(minRange, maxRange + 1);
+//     }
+//     return numbers;
+// }
     
-void PrintArray(int[] numbers)
-{
-   for (int i = 0; i < numbers.Length; i++)
-    {
-        System.Console.Write(numbers[i] + " ");
-    } 
-}
+// void PrintArray(int[] numbers)
+// {
+//    for (int i = 0; i < numbers.Length; i++)
+//     {
+//         System.Console.Write(numbers[i] + " ");
+//     } 
+// }
 
-//нахождение чисел в массиве, которые лежат в диапазоне от 10 до 90
-int FindNumber(int[] numbers)
-{
-    int count = 0;
-    for (int i = 0; i < numbers.Length; i++)
+// //нахождение чисел в массиве, которые лежат в диапазоне от 10 до 90
+// int FindNumber(int[] numbers)
+// {
+//     int count = 0;
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         if (numbers[i] > 10 && numbers[i] < 90)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+
+// System.Console.WriteLine("Введи размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введи минимальное число: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введи максимальное число: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] arr = CreateArray(size, min, max);
+// PrintArray(arr);
+// System.Console.WriteLine();
+// System.Console.WriteLine(FindNumber(arr));
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Подсчет количества элементов массива, попадающих в заданный диапазон
+    // numbers - массив, в котором ведется подсчет
+    // minRange - минимальная граница диапазона
+    // maxRange - максимальная граница диапазона
+    public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
     {
-        if (numbers[i] > 10 && numbers[i] < 90)
-        {
-            count++;
-        }
+         //Введите сюда свое решение
+      int count = 0;
+      for (int i = 0; i < numbers.Length; i++)
+      {
+          if (numbers[i] >= minRange && numbers[i] <= maxRange)
+          {
+              count++;
+          }
+      }
+      return count;
     }
-    return count;
+    
+    public static void PrintResult(int[] array)
+    {
+          //Введите сюда свое решение
+      System.Console.WriteLine(CountItemsRange(array, 10, 90));
+    }
+
 }
 
 
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+        
 
-System.Console.WriteLine("Введи размер массива: ");
-int size = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введи минимальное число: ");
-int min = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введи максимальное число: ");
-int max = Convert.ToInt32(Console.ReadLine());
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
 
-int[] arr = CreateArray(size, min, max);
-PrintArray(arr);
-System.Console.WriteLine();
-System.Console.WriteLine(FindNumber(arr));
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new int[] {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
