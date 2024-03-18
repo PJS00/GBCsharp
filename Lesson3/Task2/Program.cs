81877b10321042bc7ef3a58ef6077f8b41b00a75
PJS <b_rok9999@mail.ru>
2024-03-18 20:55:01 +0500

    Add Lesson3, Task2,3,4 Функции

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Вывод на экран квадратов чисел от 1 до N.
+// int n = 5;
+
+// int i = 1;
+// while(i <= n)
+// {
+//     System.Console.Write($"{i*i} ");
+//     i++;
+// }
+
+void PrintSquares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        System.Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
---
+++ Lesson3/Task4/Program.cs
@@ -0,0 +1,50 @@
+﻿int n = 10;
+int[] arr = new int[n];
+
+void FillArray(int[] arr)
+{
+    int i = 0;
+    while (i < arr.Length)
+    {
+        arr[i] = i + 1;
+        i++;
+    }
+}
+
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        System.Console.Write($"{e} ");
+    }
+}
+
+int GetSumOfElements(int[] arr)
+{
+    int sum = 0;
+    foreach (int e in arr)
+    {
+        sum = sum + e;
+    }
+    return sum;
+}
+
+int GetProductfElements(int[] arr)
+{
+    int product = 1;
+    foreach (int e in arr)
+    {
+        product = product * e;
+    }
+    return product;
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+int sum = GetSumOfElements(array);
+int product = GetProductfElements(array);
+System.Console.WriteLine(sum);
+System.Console.WriteLine(product);
