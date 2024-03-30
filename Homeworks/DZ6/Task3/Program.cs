602083ef0fe27df1874b87afe46e5b2f9b247c8c
PJS <b_rok9999@mail.ru>
2024-03-30 20:18:43 +0500

    Add Homeworks DZ6 Строки, массивы

---
+++ Homeworks/DZ6/Task3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+
+// bool IsPalindrom(string Str, bool ignoreCase = true)
+// {
+//     if (ignoreCase)
+//     {
+//         Str = Str.ToLowerInvariant();
+//     }
+
+//     var inputArray = Str.ToArray();
+//     var reversedArray = inputArray.Reverse().ToArray();
+
+//     return inputArray.SequenceEqual(reversedArray);
+// }
+
+// bool IsPalindrom(string Str, bool ignoreCase = true)
+// {
+//     var reversedStr = new string(Str.Reverse().ToArray());
+//     bool result;
+//     if (ignoreCase)
+//     {
+//         result = reversedStr.Equals(Str, StringComparison.InvariantCultureIgnoreCase);
+//     }
+//     else
+//     {
+//         result = reversedStr.Equals(Str);
+//     }
+
+//     return result;
+// }
+
+// Console.WriteLine("Inpute string: ");
+// string Str = Console.ReadLine();
+
+
+string Reverser(string a)
+{
+    char[] arr = a.ToCharArray();
+    Array.Reverse(arr);
+    return new string(arr);
+}
+string Palindromer(string x)
+{
+    string y = Reverser(x);
+    Console.WriteLine(y);
+    if (Equals(x, y))
+        return "Палиндром";
+    else
+        return "Не палиндром";
+}
+
+
+Console.WriteLine("Введите тестируемую строку");
+string input = Console.ReadLine();
+string str = input.ToLower();
+Console.WriteLine(input + " - " + Palindromer(str));
+Console.WriteLine();
--- Seminars/Sem7/Task3/Program.cs
+++ Seminars/Sem7/Task3/Program.cs
@@ -5,29 +5,44 @@
 // M = 1; N = 5. -> "1, 2, 3, 4, 5"
 // M = 4; N = 8. -> "4, 5, 6, 7, 8"
 
-int n = Convert.ToInt32(Console.ReadLine());
-int m = Convert.ToInt32(Console.ReadLine());
-void F(int m, int n)
+// int n = Convert.ToInt32(Console.ReadLine());
+// int m = Convert.ToInt32(Console.ReadLine());
+// void F(int m, int n)
+// {
+//   if (m != n)
+//   {
+//     if (m > n)
+//     {
+//       F(m - 1, n);
+//       System.Console.Write(m + " ");
+//     }
+//     else
+//     {
+
+//       F(m + 1, n);
+//       System.Console.Write(m + " ");
+
+//     }
+
+//   }
+//   else
+//   {
+//     System.Console.Write(m + " ");
+//   }
+// }
+
+void Fact(int m, int n)
 {
-  if (m != n)
+  if (m > n)
   {
-    if (m > n)
-    {
-      F(m - 1, n);
-      System.Console.Write(m + " ");
-    }
-    else
-    {
-
-      F(m + 1, n);
-      System.Console.Write(m + " ");
-
-    }
-
+    Fact(n, m);
   }
-  else
+  if (m == n)
   {
-    System.Console.Write(m + " ");
+    System.Console.WriteLine(m);
+    return;
   }
+  System.Console.WriteLine(m + " ");
+  Fact(m + 1, n);
 }
-  
+Fact(8, 5);
